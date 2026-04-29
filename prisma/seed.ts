fc4d78f093804acde0cae52713ecb9a29eb571ea
Orhan D <novospire@gmail.com>
2026-04-29 13:43:11 +0300

    fix: remove ts extension from prisma seed import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { Prisma } from "@prisma/client";
 
-import { prisma } from "../src/lib/prisma.ts";
+import { prisma } from "../src/lib/prisma";
 
 const TOP_NAV_CATEGORY_NAMES = [
   "Edebiyat",
